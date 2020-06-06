f83d78e027a2fb6e0740afa7ad8e0cfd5719eea2
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-06-05 17:06:27 -0700

    Implement basic repl for language

---
+++ vm.py
@@ -0,0 +1,21 @@
+from lexer import Lexer
+from parse import Parser
+from visitor import Visitor
+
+
+def main():
+    while True:
+        lexer = Lexer()
+        str_input = input(">> ")
+        tok_stream = lexer.tokenize_string(str_input)
+        parser = Parser(tok_stream)
+        root = parser.parse()
+        # print(str_input)
+        print(tok_stream)
+        v = Visitor()
+        val = v.visit(root)
+        print(val)
+
+
+if __name__ == "__main__":
+    main()
