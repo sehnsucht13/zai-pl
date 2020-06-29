ac21f179ecb5d4df6966d7a152756d5f59f6efbd
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-06-29 16:52:12 -0700

    rewrite environment tests to use new environment.

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,72 +1,53 @@
-from yapl.env import RunTime_Stack, Frame
+from yapl.env import Scope, Environment
 
 
 # Test a symbol which does not exist
 def test_sym_retrieval_DNE():
-    f = Frame()
+    f = Scope(None)
 
     # look up a non-existant symbol
-    sym = f.resolve_sym("does_not_exist")
+    sym = f.lookup_symbol("does_not_exist")
     assert sym == None
 
 
-def test_sym_retrieval_exists():
-    f = Frame()
+def test_scope_overwrite_sym():
+    f = Scope(None)
 
     # look up an existing symbol
     f.add_symbol("sym", 4)
-    sym = f.resolve_sym("sym")
+    sym = f.lookup_symbol("sym")
     assert sym == 4
 
-    # Test nested scope resolution
-    f.enter_scope()
-    sym = f.resolve_sym("sym")
-    assert sym == 4
-
-    # Test variable name shadowing
     f.add_symbol("sym", 13)
-    sym = f.resolve_sym("sym")
+    sym = f.lookup_symbol("sym")
     assert sym == 13
 
 
 def test_scope_nesting():
-    f = Frame()
+    e = Environment()
+    e.peek().add_symbol("sym_outer", 13)
+    sym = e.peek().lookup_symbol("sym_outer")
+    assert sym == 13
 
-    f.add_symbol("sym_a", 1)
+    e.enter_scope(e.peek())
+    e.peek().add_symbol("sym_inner", 2)
 
-    f.enter_scope()
-    f.add_symbol("sym_b", 2)
+    sym_inner = e.peek().lookup_symbol("sym_inner")
+    assert sym_inner == 2
+    sym_outer = e.peek().lookup_symbol("sym_outer")
+    assert sym_outer == 13
 
-    f.enter_scope()
-    f.add_symbol("sym_c", 3)
 
-    sym = f.resolve_sym("sym_c")
-    assert sym == 3
-    sym = f.resolve_sym("sym_b")
+def test_scope_shadowing():
+    e = Environment()
+
+    e.peek().add_symbol("sym", 13)
+
+    e.enter_scope(e.peek())
+    e.peek().add_symbol("sym", 2)
+
+    sym = e.peek().lookup_symbol("sym")
     assert sym == 2
-    sym = f.resolve_sym("sym_a")
-    assert sym == 1
-
-    f.exit_scope()
-    sym = f.resolve_sym("sym_c")
-    assert sym == None
-    sym = f.resolve_sym("sym_b")
-    assert sym == 2
-    sym = f.resolve_sym("sym_a")
-    assert sym == 1
-
-    f.exit_scope()
-    sym = f.resolve_sym("sym_c")
-    assert sym == None
-    sym = f.resolve_sym("sym_b")
-    assert sym == None
-    sym = f.resolve_sym("sym_a")
-    assert sym == 1
-
-    f.exit_scope()
-    sym = f.resolve_sym("sym_c")
-    assert sym == None
-    sym = f.resolve_sym("sym_b")
-    assert sym == None
-    sym = f.resolve_sym("sym_a")
-    assert sym == None
+    e.exit_scope()
+    sym = e.peek().lookup_symbol("sym")
+    assert sym == 13
