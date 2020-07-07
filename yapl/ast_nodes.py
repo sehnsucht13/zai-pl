d8dbc856dd61035240415d10287c6cb27aeb0344
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-07-06 19:32:04 -0700

    Add AST node representing "." operator.

--- yapl/ast_nodes.py
+++ yapl/ast_nodes.py
@@ -46,6 +46,21 @@ class Bin_Node(AST_Node):
         raise NotImplementedError()
 
 
+class Dot_Bin_Node(AST_Node):
+    def __init__(self, left, right):
+        self.object_name = left
+        self.prop_name = right
+
+    def __str__(self):
+        return "DOT_NODE {} {}".format(self.object_name, self.prop_name)
+
+    def __repr__(self):
+        return "DOT_NODE {} {}".format(self.object_name, self.prop_name)
+
+    def accept(self, visitor):
+        return visitor.visit_dot_node(self)
+
+
 class Replace_Assign_Bin_Node(AST_Node):
     def __init__(self, symbol, value, local_assign=False):
         self.symbol = symbol
