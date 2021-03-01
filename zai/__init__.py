37084df41d53d13a27f4fb52ceaf2b7ae9fd73a2
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-28 18:16:50 -0800

    Add licence to files.

---
+++ zai/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 by Yavor Konstantinov <ykonstantinov1@gmail.com>
+
+# This file is part of zai-pl.
+
+# zai-pl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# zai-pl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with zai-pl. If not, see <https://www.gnu.org/licenses/>.
